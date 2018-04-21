0f1917edfebceef13bdfcb72ecbdbfd9f130d1ba
Stephan Maihoefer <stephan@undev.de>
2018-04-21 14:11:21 +0200

    remove vendor folder again...

--- vendor/composer/installers/src/Composer/Installers/ElggInstaller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Composer\Installers;
-
-class ElggInstaller extends BaseInstaller
-{
-    protected $locations = array(
-        'Updater' => 'mod/{$name}/',
-    );
-}
--- vendor/composer/installers/src/Composer/Installers/WolfCMSInstaller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Composer\Installers;
-
-class WolfCMSInstaller extends BaseInstaller
-{
-    protected $locations = array(
-        'Updater' => 'wolf/plugins/{$name}/',
-    );
-}

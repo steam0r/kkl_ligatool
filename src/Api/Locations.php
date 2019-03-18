9179a9ed09dbcd4724e5a95050f27c7bccd56495
Stephan Maihoefer <stephan@undev.de>
2019-03-18 12:14:55 +0100

    fix bug in teams for location

--- src/Api/Locations.php
+++ src/Api/Locations.php
@@ -9,8 +9,24 @@ use WP_REST_Server;
 class Locations extends Controller {
   
   public function register_routes() {
-    register_rest_route($this->getNamespace(), '/' . $this->getBaseName(), array('methods' => WP_REST_Server::READABLE, 'callback' => array($this, 'get_locations'), 'args' => array(),));
-    register_rest_route($this->getNamespace(), '/' . $this->getBaseName() . '/(?P<id>[\d]+)', array('methods' => WP_REST_Server::READABLE, 'callback' => array($this, 'get_location'), 'args' => array('context' => array('default' => 'view',),)));
+    register_rest_route(
+      $this->getNamespace(),
+      '/' . $this->getBaseName(),
+      array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => array($this, 'get_locations'),
+        'args' => array()
+      )
+    );
+    register_rest_route(
+      $this->getNamespace(),
+      '/' . $this->getBaseName() . '/(?P<id>[\d]+)',
+      array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => array($this, 'get_location'),
+        'args' => array('context' => array('default' => 'view'))
+      )
+    );
     register_rest_route($this->getNamespace(), '/' . $this->getBaseName() . '/(?P<id>[\d]+)/teams', array('methods' => WP_REST_Server::READABLE, 'callback' => array($this, 'get_teams_for_location'), 'args' => array('context' => array('default' => 'view',),)));
   }
   

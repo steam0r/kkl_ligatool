5495bf0d21c4ec62b0b7132a11602fe3d0d30019
Stephan Maihoefer <stephan@undev.de>
2020-02-11 20:36:03 +0100

    more abstract test setup

--- test/php/Services/KKLModelServiceTest.php
+++ test/php/Services/KKLModelServiceTest.php
@@ -2,8 +2,29 @@
 
 namespace Services;
 
+use KKL\Ligatool\ServiceBroker;
+use KKL\Ligatool\Services\KKLModelService;
 use PHPUnit\Framework\TestCase;
 
 abstract class KKLModelServiceTest extends TestCase {
+
+  /**
+   * @var KKLModelService
+   */
+  protected $service;
+
+  protected function setUp(): void {
+    ServiceBroker::init('TEST');
+  }
+
+  protected abstract function initData();
+
+  /*
+  public function testById() {
+    $model = $this->service->byId(1);
+    $this->assertNotNull($model);
+  }
+  */
+
 
 }
--- test/php/Services/LeagueServiceTest.php
+++ test/php/Services/LeagueServiceTest.php
@@ -2,19 +2,30 @@
 
 namespace Services;
 
-use KKL\Ligatool\Mocks\LeagueServiceMock;
+use KKL\Ligatool\Model\League;
 use KKL\Ligatool\ServiceBroker;
 use KKL\Ligatool\Services\LeagueService;
 
 class LeagueServiceTest extends KKLModelServiceTest {
 
+  /**
+   * @var LeagueService
+   */
+  protected $service;
+
   protected function setUp(): void {
-    ServiceBroker::init('TEST');
+    parent::setUp();;
+    $this->service = ServiceBroker::getLeagueService();
+    $this->assertInstanceOf(LeagueService::class, $this->service);
   }
 
-  public function testServiceCreation() {
-    $leagueService = ServiceBroker::getLeagueService();
-    $this->assertInstanceOf(LeagueService::class, $leagueService);
+  protected function initData() {
+    $league1 = new League();
+    $league1->setActive(true);
+
+    $league2 = new League();
+    $league2->setActive(false);
   }
+
 
 }
